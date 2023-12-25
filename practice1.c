3227e94b39e261d5093a464be3810f5fd8c0f34f
NULL <NULL>
2023-12-25 04:20:13 +0200

    working file practice1.c

---
+++ practice1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n,sum,i,j;
+    scanf("%d", &n);
+
+    //Complete the code to calculate the sum of the five digits on n.
+	printf("n%%10: %d\n",n%10);
+	
+    return 0;
+}
